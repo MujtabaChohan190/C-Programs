60718324018e1d3c8f67b2b0c32caa45ef0378f0
Muhammad Mujtaba Chohan <mujtabachohan5@gmail.com>
2024-11-09 13:17:01 +0500

    Demonstrating size difference between int pointer and array pointer

---
+++ Pointers/pointer_size.c
@@ -0,0 +1,24 @@
+// C program to illustrate sizes of
+// pointer of array
+#include<stdio.h>
+
+int main()
+{
+    int arr[] = { 3, 5, 6, 7, 9 };
+    int *p = arr;
+    int (*ptr)[5] = &arr;
+    
+    printf("p = %p, ptr = %p\n", p, ptr);
+    printf("*p = %d, *ptr = %p\n", *p, *ptr);
+    
+    printf("sizeof(p) = %lu, sizeof(*p) = %lu\n",
+                          sizeof(p), sizeof(*p));
+    printf("sizeof(ptr) = %lu, sizeof(*ptr) = %lu\n", 
+                        sizeof(ptr), sizeof(*ptr));
+    return 0;
+}
+
+// p = 0x7fff55adbff0, ptr = 0x7fff55adbff0
+// *p = 3, *ptr = 0x7fff55adbff0
+// sizeof(p) = 8, sizeof(*p) = 4
+// sizeof(ptr) = 8, sizeof(*ptr) = 20
